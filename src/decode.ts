6b9db60c1ae7249b1f07fc2cb81cbf92ab084f60
Sʜɪᴍᴜʀᴀ Yū <mail@yuhr.org>
2025-08-03 09:17:44 +0900

    feat: add `encode` and `decode`

---
+++ src/decode.ts
@@ -0,0 +1,34 @@
+import type ByteSource from "./ByteSource.ts"
+import fromBytes from "./fromBytes.ts"
+import toValue from "./toValue.ts"
+
+/**
+ * Decodes a CBOR representation into a corresponding bare value.
+ *
+ * `await decode(source)` is equivalent to `await toValue(await fromBytes(source, { allowEmpty: false }))`.
+ */
+const decode = async (source: ByteSource, options: decode.Options = {}): Promise<unknown> => {
+	const specifiedAllowEmptyAsUndefined =
+		options.fromBytes !== undefined &&
+		"allowEmpty" in options.fromBytes &&
+		// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+		options.fromBytes.allowEmpty === undefined
+	if (specifiedAllowEmptyAsUndefined)
+		throw new Error("Setting `allowEmpty` to `undefined` is unsupported in `decode`.")
+
+	const dataItem = await fromBytes(source, {
+		...options.fromBytes,
+		allowEmpty: options.fromBytes?.allowEmpty ?? false,
+	})
+	if (!dataItem) return undefined
+	return await toValue(dataItem, options.toValue)
+}
+
+namespace decode {
+	export type Options = {
+		toValue?: toValue.Options
+		fromBytes?: fromBytes.Options & { allowEmpty?: boolean }
+	}
+}
+
+export default decode
---
+++ src/encode.ts
@@ -0,0 +1,19 @@
+import fromValue from "./fromValue.ts"
+import toBytes from "./toBytes.ts"
+
+/**
+ * Encodes a bare value into a corresponding CBOR representation.
+ *
+ * `await encode(value)` is equivalent to `await toBytes(await fromValue(value))`.
+ */
+const encode = async (value: unknown, options: encode.Options = {}): Promise<Uint8Array> =>
+	await toBytes(await fromValue(value, options.fromValue), options.toBytes)
+
+namespace encode {
+	export type Options = {
+		toBytes?: toBytes.Options
+		fromValue?: fromValue.Options
+	}
+}
+
+export default encode
